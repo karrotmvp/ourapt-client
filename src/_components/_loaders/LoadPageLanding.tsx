04cc6a742d270b6240f5fbfcec3577f5186a6f54
chloee <chloeelog@gmail.com>
2021-11-15 16:53:04 +0900

    update: set LoadPageLanding

--- src/_components/_loaders/LoadPageLanding.tsx
+++ src/_components/_loaders/LoadPageLanding.tsx
@@ -1,12 +1,83 @@
 import React from "react";
 
+import styled from "@emotion/styled";
+
 const LoadPageLanding: React.FC = () => {
   return (
     <div className="Page">
-      <div>스크린헬멧 영역</div>
-      <div>페이지 랜딩 타이틀</div>
+      <LoadScreenHelmet></LoadScreenHelmet>
+      <div className="pd--24">
+        <LoadLandingTitle_FirstLine />
+        <LoadLandingTitle_SecondLine />
+        <LoadBrandWrapper>
+          <LoadBrandTitle />
+          <LoadBrandHorizon />
+        </LoadBrandWrapper>
+        <LoadApartmentInLanding className="loaderItem-first" />
+        <LoadApartmentInLanding className="loaderItem-second" />
+        <LoadApartmentInLanding className="loaderItem-third" />
+        <LoadApartmentInLanding className="loaderItem-fourth" />
+      </div>
     </div>
   );
 };
 
 export default LoadPageLanding;
+
+const LoadScreenHelmet = styled.div`
+  width: 100%;
+  height: 56px;
+  border-bottom: 1px solid #f5f5f5;
+`;
+
+const LoadLandingTitle_FirstLine = styled.div`
+  height: 24px;
+  width: 130px;
+
+  margin-bottom: 12px;
+
+  background-color: #f5f5f5;
+`;
+const LoadLandingTitle_SecondLine = styled.div`
+  height: 24px;
+  width: 240px;
+
+  margin-bottom: 24px;
+  background-color: #f5f5f5;
+`;
+
+const LoadBrandWrapper = styled.div`
+  margin-bottom: 8px;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const LoadBrandTitle = styled.div`
+  width: 30px;
+  height: 15px;
+
+  background-color: #f5f5f5;
+`;
+
+const LoadBrandHorizon = styled.div`
+  height: 1px;
+  width: 268px;
+
+  margin-right: 0;
+
+  border: none;
+
+  background-color: #f5f5f5;
+`;
+
+const LoadApartmentInLanding = styled.div`
+  width: 100%;
+  height: 54px;
+
+  margin-bottom: 16px;
+
+  border-radius: 8px;
+`;
