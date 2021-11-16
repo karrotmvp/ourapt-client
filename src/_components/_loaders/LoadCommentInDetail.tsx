23a75048d6153afb2b2aa34d5b624100811ec9eb
chloee <chloeelog@gmail.com>
2021-11-16 11:23:41 +0900

    wip: working in progress

---
+++ src/_components/_loaders/LoadCommentInDetail.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styled from "@emotion/styled";
+
+const LoadCommentInDetail: React.FC = () => {
+  return <div>로딩</div>;
+};
+
+export default LoadCommentInDetail;
