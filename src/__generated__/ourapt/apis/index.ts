0cdd92619567ad9aa0fe52263de337d0051e889c
chloee <chloeelog@gmail.com>
2021-11-19 12:42:06 +0900

    update: generating VoteControllerApi

--- src/__generated__/ourapt/apis/index.ts
+++ src/__generated__/ourapt/apis/index.ts
@@ -1,11 +1,10 @@
 /* tslint:disable */
 /* eslint-disable */
-export * from './DefaultApi';
 export * from './Class1APIApi';
 export * from './Class2Api';
 export * from './Class3Api';
-export * from './Class4Api';
-export * from './Class4ArticleApi';
+export * from './Class41Api';
+export * from './Class42Api';
 export * from './Class5Api';
 export * from './Class6Api';
 export * from './Class7Api';
