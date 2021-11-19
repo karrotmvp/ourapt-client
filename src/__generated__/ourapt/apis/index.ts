e8c7f22dbbfdcfc360ba99088ba9c263dc364ee3
chloee <chloeelog@gmail.com>
2021-11-19 12:31:51 +0900

    wip: updating swagger...

--- src/__generated__/ourapt/apis/index.ts
+++ src/__generated__/ourapt/apis/index.ts
@@ -1,10 +1,13 @@
 /* tslint:disable */
 /* eslint-disable */
+export * from './DefaultApi';
 export * from './Class1APIApi';
 export * from './Class2Api';
 export * from './Class3Api';
 export * from './Class4Api';
+export * from './Class4ArticleApi';
 export * from './Class5Api';
 export * from './Class6Api';
 export * from './Class7Api';
 export * from './Class99Api';
+export * from './ControllerApi';
